fe1d7bc0d7c2826cc4e97982890af57cd0e37d31
savaki <matt.ho@gmail.com>
2014-08-21 00:19:48 -0700

    - added cors filter

--- src/main/scala/todo/server/TodoServer.scala
+++ src/main/scala/todo/server/TodoServer.scala
@@ -1,10 +1,26 @@
 package todo.server
 
+import com.twitter.finagle.http.{Request => FinagleRequest, Response => FinagleResponse}
+import com.twitter.finagle.{Filter, Service}
 import com.twitter.finatra.FinatraServer
+import com.twitter.util.Future
 import todo.api.DefaultTodoApi
 import todo.controller.TodoController
 
+class CorsFilter extends Filter[FinagleRequest, FinagleResponse, FinagleRequest, FinagleResponse] {
+  override def apply(request: FinagleRequest, service: Service[FinagleRequest, FinagleResponse]): Future[FinagleResponse] = {
+    request
+      .headers()
+      .add("access-control-allow-origin", "*")
+      .add("access-control-allow-headers", "accept, content-type")
+      .add("access-control-allow-methods", "GET,HEAD,POST,DELETE,OPTIONS,PUT,PATCH")
+    service(request)
+  }
+}
+
 class TodoServer extends FinatraServer {
+  addFilter(new CorsFilter)
+
   val controller = new TodoController(new DefaultTodoApi)
   register(controller)
 }

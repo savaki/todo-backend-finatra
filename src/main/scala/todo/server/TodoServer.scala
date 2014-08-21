7f07101223f2f1f9859e945ba9fca0d1ae6c120c
savaki <matt.ho@gmail.com>
2014-08-21 01:28:06 -0700

    - added logging filter

--- src/main/scala/todo/server/TodoServer.scala
+++ src/main/scala/todo/server/TodoServer.scala
@@ -1,11 +1,15 @@
 package todo.server
 
+import java.nio.charset.Charset
+
 import com.twitter.finagle.http.{Request => FinagleRequest, Response => FinagleResponse}
 import com.twitter.finagle.{Filter, Service}
 import com.twitter.finatra.FinatraServer
 import com.twitter.util.Future
+import org.jboss.netty.buffer.ChannelBuffer
 import todo.api.DefaultTodoApi
 import todo.controller.TodoController
+import scala.collection.JavaConversions._
 
 class CorsFilter extends Filter[FinagleRequest, FinagleResponse, FinagleRequest, FinagleResponse] {
   override def apply(request: FinagleRequest, service: Service[FinagleRequest, FinagleResponse]): Future[FinagleResponse] = {
@@ -21,8 +25,48 @@ class CorsFilter extends Filter[FinagleRequest, FinagleResponse, FinagleRequest,
   }
 }
 
+class LoggingFilter extends Filter[FinagleRequest, FinagleResponse, FinagleRequest, FinagleResponse] {
+  override def apply(request: FinagleRequest, service: Service[FinagleRequest, FinagleResponse]): Future[FinagleResponse] = {
+    // REQUEST
+    println("# Request ---------------------------------------------------------------")
+    println(s"${request.getMethod()} ${request.getUri()}")
+    request.headers().entries().map {
+      case entry => println(f"${entry.getKey}%30s: ${entry.getValue}")
+    }
+    println("")
+    println(toString(request.getContent()))
+
+    // SERVICE
+    val future: Future[FinagleResponse] = service(request)
+
+    // RESPONSE
+    println("# Response --------------------------------------------------------------")
+    future.map {
+      response => {
+        response.headerMap.map {
+          case (key, value) => println(f"$key%30s: $value")
+        }
+
+        println("")
+        println(toString(response.getContent()))
+      }
+    }
+
+    future
+  }
+
+  def toString(content: ChannelBuffer): String = {
+    if (content != null) {
+      content.copy().toString(Charset.defaultCharset())
+    } else {
+      ""
+    }
+  }
+}
+
 class TodoServer extends FinatraServer {
   addFilter(new CorsFilter)
+  addFilter(new LoggingFilter)
 
   val controller = new TodoController(new DefaultTodoApi)
   register(controller)
